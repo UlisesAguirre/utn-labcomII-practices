3a6884bb686e4fc7480dfc53283a21c1b65b0aef
UlisesAguirre <ulises.m.aguirre@hotmail.com>
2022-09-21 12:41:12 -0300

    Añade practicas unidad 4

---
+++ U04P04/js/app.js
@@ -0,0 +1,34 @@
+var contLocal = localStorage.getItem('click') || 0;
+var contSession = sessionStorage.getItem('click') || 0;
+
+function contador() {
+    var contador = document.getElementById('valorLocal')
+    if (contLocal == 0) {
+        contLocal = 1
+        localStorage.setItem("click", contLocal)   
+    } else {
+        contLocal++
+        localStorage.setItem("click", contLocal)
+    }
+    //Estilo
+    contador.style.border = "1px solid #000000"
+    contador.style.borderRadius = "15px"
+    contador.innerText = contLocal
+}
+
+//No funciona esta funcion
+function contador2() {
+    var contador = document.getElementById('valorSession')
+    if (contSession == 0) {
+        contSession = 1
+        sessionStorage.setItem("click", contSession)   
+    } else {
+        contSession++
+        sessionStorage.setItem("click", contSession)
+    }
+    //Estilo
+    contador.style.border = "1px solid #000000"
+    contador.style.borderRadius = "15px"
+    contador.innerText = contSession
+}
+
